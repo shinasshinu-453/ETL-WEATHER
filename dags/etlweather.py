830246eff322a4a4ae6b504cb75a0ce3d987f705
shinasshinu-453 <shinasshinu453@gmail.com>
2025-12-16 01:28:51 +0530

    Initial

---
+++ dags/etlweather.py
@@ -0,0 +1,92 @@
+from airflow import DAG
+from airflow.providers.http.hooks.http import HttpHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.decorators import task
+from airflow.utils.dates import days_ago
+
+# Constants
+latitude = '10.4597'
+longitude = '76.5625'
+POSTGRES_CONN_ID = 'postgres_supabase'
+API_CONN_ID = 'open_meteo_api'
+
+# DAG Default Arguments
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+    'email': ['shinasshinu453@gmail.com']
+}
+
+# Define the DAG
+with DAG(
+    dag_id='weather_etl_pipeline',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+    tags=["weather"]
+) as dag:
+
+    @task()
+    def get_weather_data():
+        """Extract weather data using HTTP Hook"""
+        http_hook = HttpHook(http_conn_id=API_CONN_ID, method='GET')
+        endpoint = f'/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true'
+
+        response = http_hook.run(endpoint)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to fetch weather data: {response.status_code}")
+
+    @task()
+    def transform_weather_data(weather_data):
+        """Transform the extracted weather data."""
+        current_weather = weather_data['current_weather']
+        transformed_data = {
+            'latitude': float(latitude),
+            'longitude': float(longitude),
+            'temperature': current_weather['temperature'],
+            'windspeed': current_weather['windspeed'],
+            'winddirection': current_weather['winddirection'],
+            'weathercode': current_weather['weathercode']
+        }
+        return transformed_data
+
+    @task()
+    def load_weather_data(transformed_data):
+        """Load transformed data into PostgreSQL."""
+        pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+        conn = pg_hook.get_conn()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS weather_data (
+            latitude FLOAT,
+            longitude FLOAT,
+            temperature FLOAT,
+            windspeed FLOAT,
+            winddirection FLOAT,
+            weathercode INT,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        """)
+
+        cursor.execute("""
+        INSERT INTO weather_data (latitude, longitude, temperature, windspeed, winddirection, weathercode)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        """, (
+            transformed_data['latitude'],
+            transformed_data['longitude'],
+            transformed_data['temperature'],
+            transformed_data['windspeed'],
+            transformed_data['winddirection'],
+            transformed_data['weathercode']
+        ))
+
+        conn.commit()
+        cursor.close()
+
+    # Define task dependencies
+    raw_data = get_weather_data()
+    processed_data = transform_weather_data(raw_data)
+    load_weather_data(processed_data)
